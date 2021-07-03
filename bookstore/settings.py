f72c6936ed6198afc76ca5f17763b9d4652e242e
Shrutika <shrutika01234@gmail.com>
2021-07-03 19:01:31 +0530

    edit

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'hqzk!brea*snwz+xm#)bkq$*@)%mbmze)dr=+ji9qt!4*wyuc('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
